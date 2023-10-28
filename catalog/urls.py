89885ea077fffd548adeb1aa52862e2edfdeee80
EkMakarenko <makarenkoek@yandex.ru>
2023-10-28 22:45:04 +0300

    fix 1

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,9 @@ from catalog import views
 
 urlpatterns = [
       path('test/', views.read_information, name='read_information'),
-      path('info/', views.InfoCatalogView.as_view(), name='info'),
-      path('info/<int:id>', views.InfoCatalogView.as_view(), name='information')
+      path('info/', views.InfoCatalogListView.as_view(), name='info_list'),
+      path('info/<int:pk>', views.InfoCatalogDetailView.as_view(), name='info-detail'),
+      # path('info/<int:pk>/update', views.PostUpdateView.as_view(), name='info-update'),
+      # path('info/<int:pk>/delete', views.PostDeleteView.as_view(), name='info-delete'),
+      # path('info/create', views.PostCreateView.as_view(), name='info-create'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,5 @@
-from gettext import Catalog
-
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.views import View
+from django.views import generic
 
 from catalog.models import HotelCatalog
 
@@ -14,12 +11,59 @@ def read_information(request):
     return render(request, 'index.html')
 
 
-class InfoCatalogView(View):
-    def get(self, request, id=None):
-        info = HotelCatalog.objects.all()
+# class InfoCatalogView(View):
+#     def get(self, request, id=None):
+#         info = HotelCatalog.objects.all()
+#
+#         if id:
+#             information = HotelCatalog.objects.get(id=id, is_deleted=False)
+#             return render(request, 'info.html', context={'info': info, 'information': information})
+#
+#         return render(request, 'index.html', context={'info': info})
 
-        if id:
-            information = HotelCatalog.objects.get(id=id, is_deleted=False)
-            return render(request, 'info.html', context={'info': info, 'information': information})
+class InfoCatalogListView(generic.ListView):
+    template_name = 'info_catalog/info_list.html'
+    context_object_name = 'info'
+    queryset = HotelCatalog.objects.filter(is_deleted=False)
 
-        return render(request, 'index.html', context={'info': info})
+
+class PostCreateView(generic.CreateView):
+    pass
+#     # model = InfoBlog
+#     template_name = 'info_blog/post_create.html'
+#     form_class = InfoBlogForm
+#     # fields = ('name', 'text', 'rating', 'price')
+#     # success_url = '/blog/posts/'
+#
+#     def get_success_url(self):
+#         return reverse('post-list')
+
+
+class InfoCatalogDetailView(generic.DetailView):
+    pass
+    model = HotelCatalog
+    template_name = 'info_catalog/info_detail.html'
+    context_object_name = 'info'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['posts'] = InfoBlog.objects.filter(is_deleted=False)
+#         # print(50*'-')
+#         # print(context['posts'].values()[0]['name'])
+#         # print(context['posts'].query)
+#         # print(50*'-')
+#         return context
+#
+#
+class PostUpdateView(generic.UpdateView):
+    pass
+#     model = InfoBlog
+#     template_name = 'info_blog/post_update.html'
+#     form_class = InfoBlogForm
+#
+#     def get_success_url(self):
+#         return reverse('post-list')
+#
+#
+class PostDeleteView(generic.DeleteView):
+    pass
